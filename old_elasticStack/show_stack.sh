7272ab4ab9b0ce353924bdb2b4c116b87c57f114
cs <christoph.schranz@salzburgresearch.at>
2019-05-23 23:16:16 +0200

    Restructured such that the adapterfiles are in datastack-adapter/adapter and the pr client is used

--- old_elasticStack/show_stack.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "Printing 'docker service ls | grep stack':"
-docker service ls | grep stack
-echo ""
-echo "Printing 'docker stack ps stack':"
-docker stack ps stack
--- old_elasticStack/start_stack.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-sudo docker-compose build
-sudo docker-compose push || true
-sudo docker stack deploy --compose-file docker-compose.yml stack
