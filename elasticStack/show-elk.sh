3ab46ea6f8534454a3e95a1d3b52b7e4dd90492a
cs <christoph.schranz@salzburgresearch.at>
2019-06-13 13:17:55 +0200

    Reduced index to monthly

--- elasticStack/show-elk.sh
+++ elasticStack/show-elk.sh
@@ -3,4 +3,4 @@ echo "Printing 'docker service ls | grep elk':"
 docker service ls | grep elk
 echo ""
 echo "Printing 'docker stack ps elk':"
-docker stack ps elk
+docker stack ps elk | grep Running
