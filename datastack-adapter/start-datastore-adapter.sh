5bc94acdeb7a5851a8032e005839267771e76a44
Christoph Schranz <christoph.schranz@salzburgresearch.at>
2019-07-29 17:16:18 +0200

    set directory to scriptname

--- datastack-adapter/start-datastore-adapter.sh
+++ datastack-adapter/start-datastore-adapter.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd $(cd -P -- "$(dirname -- "$0")" && pwd -P)
 docker-compose build
 docker-compose push || true
 docker stack deploy --compose-file docker-compose.yml add-datastore
